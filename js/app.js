98ef516eb544b8e3032e91d6f69b4198ee7193bf
Jesse Cravens <jesse.cravens@frogdesign.com>
2013-06-18 16:30:00 -0500

    minor updates

--- js/app.js
+++ js/app.js
@@ -1,56 +1,59 @@
-
 if (typeof PC == "undefined" || !PC) {
-  var PC = {};
+	var PC = {};
 }
 
 PC.BballLeague = Ember.Application.create({
-  LOG_TRANSITIONS: true
+	LOG_TRANSITIONS: true
 });
 
-PC.BballLeague.Router.map(function() {
-  this.route("home", { path: "/" });
-  this.resource('team', { path: '/team/:team_id' });
+PC.BballLeague.Router.map(function () {
+	this.route("home", {
+		path: "/"
+	});
+	this.resource('team', {
+		path: '/team/:team_id'
+	});
 });
 
 PC.BballLeague.Store = DS.Store.extend({
-  revision: 12,
-  adapter: 'DS.FixtureAdapter'
+	revision: 12,
+	adapter: 'DS.FixtureAdapter'
 });
 
 PC.BballLeague.Team = DS.Model.extend({
-  name: DS.attr('string'),
-  colors: DS.attr('string')
+	name: DS.attr('string'),
+	colors: DS.attr('string')
 })
 
 PC.BballLeague.HomeRoute = Ember.Route.extend({
-  model: function(){
-   return ['Coed', 'Saturdays from 9AM-3PM', 'at Park Center']
+	model: function () {
+		return ['Coed', 'Saturdays from 9AM-3PM', 'at Park Center']
 
-  },
-  setupController: function(controller, model){
-    controller.set('info', model)
-    controller.set('teams', PC.BballLeague.Team.find());
-  }
+	},
+	setupController: function (controller, model) {
+		controller.set('info', model)
+		controller.set('teams', PC.BballLeague.Team.find());
+	}
 });
 
 PC.BballLeague.Team.FIXTURES = [{
-  id: 1,
-  name: 'Celtics',
-  colors: 'Green, White'
-}, {
-  id: 2,
-  name: 'Lakers',
-  colors: 'Yellow, Black'
-}, {
-  id: 3,
-  name: 'Bulls',
-  colors: 'Red, Black'
-}, {
-  id: 4,
-  name: 'Mavericks',
-  colors: 'Blue, White'
-}, {
-  id: 5,
-  name: 'Spurs',
-  colors: 'Black, Grey, White'
-}];
+		id: 1,
+		name: 'Celtics',
+		colors: 'Green, White'
+	}, {
+		id: 2,
+		name: 'Lakers',
+		colors: 'Yellow, Black'
+	}, {
+		id: 3,
+		name: 'Bulls',
+		colors: 'Red, Black'
+	}, {
+		id: 4,
+		name: 'Mavericks',
+		colors: 'Blue, White'
+	}, {
+		id: 5,
+		name: 'Spurs',
+		colors: 'Black, Grey, White'
+	}];
